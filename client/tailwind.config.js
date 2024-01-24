2c3e27c0a7dbf37afcef5a8dcbc8d96032dd335e
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-01-24 19:40:58 +0000

    Apply custom background color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,13 +4,17 @@ export default {
     './src/pages/**/*.{html,ts,tsx,js,jsx}',
     './src/components/**/*.{html,ts,tsx,js,jsx}',
     './src/**/*.{html,ts,tsx,js,jsx}',
-    './src/index.html',
+    './index.html',
   ],
   theme: {
     fontFamily: {
       heading: ['"Russo One"', 'serif'],
       body: ['"Source Sans 3"', 'sans-serif'],
       special: ['"Ruslan Display"', 'serif'],
+    },
+    backgroundImage: {
+      'custom-gradient':
+        'linear-gradient(#4E3636 10%, #775A5A 50%, #4E3636 90%)',
     },
     extend: {},
   },
