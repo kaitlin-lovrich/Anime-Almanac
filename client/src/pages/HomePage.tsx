19e8ff8915347fd7883e7b77cc95b0fdf969be3e
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-02-01 00:43:43 +0000

    Implement a timeout function to solve development rate-limit issue, timeout for second api call can be removed later

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -7,33 +7,51 @@ export default function HomePage() {
   const [genres, setGenres] = useState<GenreType[]>([]);
 
   useEffect(() => {
+    // Array to hold all timeout IDs
+    const timerIds: (number | NodeJS.Timeout)[] = [];
+
     async function loadHomePage() {
       try {
-        const removeGenres = [
-          // 'Action',
+        const initialGenres = [
+          'Action',
           'Award Winning',
           'Adventure',
-          'Comedy',
-          'Drama',
-          'Fantasy',
-          'Horror',
-          'Mystery',
-          'Romance',
-          'Sci-Fi',
-          'Slice of Life',
-          'Supernatural',
-          'Suspense',
+          // 'Comedy',
+          // 'Drama',
+          // 'Fantasy',
+          // 'Horror',
+          // 'Mystery',
+          // 'Romance',
+          // 'Sci-Fi',
+          // 'Slice of Life',
+          // 'Supernatural',
+          // 'Suspense',
         ];
-        // Filter out genres that are in the removeGenres array to reduce number of api calls
-        const filteredGenres = genreData.filter(
-          (genre: GenreType) => !removeGenres.includes(genre.name)
+        // Immediately set the first genre to avoid rate limit.
+        const firstGenre = genreData.find(
+          (genre) => genre.name === initialGenres[0]
         );
-        setGenres(filteredGenres);
+        if (firstGenre) setGenres([firstGenre]); // If firstGenre found, set it
+
+        // Set second genre after a delay
+        const timerId1 = setTimeout(() => {
+          const secondGenre = genreData.find(
+            (genre) => genre.name === initialGenres[1]
+          );
+          if (secondGenre) {
+            setGenres((prevGenres) => [...prevGenres, secondGenre]);
+          }
+        }, 1000); // Delay of 1 second for the second genre
+        timerIds.push(timerId1);
       } catch (err) {
         console.error(`Error loading home page: ${err}`);
       }
     }
     loadHomePage();
+    // Cleanup function to clear all timeouts
+    return () => {
+      timerIds.forEach((timerId) => clearTimeout(timerId as number));
+    };
   }, []);
 
   return (
