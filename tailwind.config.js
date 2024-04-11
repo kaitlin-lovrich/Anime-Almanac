8866bad84dd2d68ed31df30d4cc9c3420f635fd4
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-04-11 16:51:16 -0700

    Coppied working portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    content: ["./src/**/*.{html,ts,tsx,js,jsx}"],
     theme: {
         fontFamily: {
             heading: ['"Russo One"', "serif"],
