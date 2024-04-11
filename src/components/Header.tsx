0629689656a874f60212a2b71cf0f77bb750fef7
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-04-11 15:23:15 -0700

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,12 @@ export default function Header({ children }: HeaderProps) {
     return (
         <>
             <nav className="flex fixed top-0 w-full z-1 justify-between bg-custom-gradient-2 shadow-custom-drop">
-                <div className="w-72 m-2">
+                {/* <div className="w-72 m-2">
                     <img
                         src="Anime-Almanacs-logo.png"
                         className="w-full drop-shadow-md"
                     />
-                </div>
+                </div> */}
 
                 <div>
                     <h2>Search</h2>
