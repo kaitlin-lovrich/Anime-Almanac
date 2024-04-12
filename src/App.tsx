b75794fc029cb2003ba1086863c9d509ec14f89a
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-04-12 15:39:36 -0700

    Added all files back, Program works completely on localhost

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-// import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
 
 export default function App() {
     return (
-        <Header>
-            <HomePage />
-        </Header>
+        <Routes>
+            <Route path="/" element={<Header />}>
+                <Route index element={<HomePage />} />
+            </Route>
+        </Routes>
     );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,39 @@
-type HeaderProps = {
-    children: React.ReactNode;
-};
+import { Link, Outlet } from "react-router-dom";
 
-export default function Header({ children }: HeaderProps) {
+export default function Header() {
     return (
         <>
-            <nav className="flex fixed top-0 w-full z-1 justify-between bg-custom-gradient-2 shadow-custom-drop">
-                {/* <div className="w-72 m-2">
-                    <img
-                        src="Anime-Almanacs-logo.png"
-                        className="w-full drop-shadow-md"
-                    />
-                </div> */}
-
+            <nav className="flex fixed top-0 w-full z-50 justify-between bg-custom-gradient-2 shadow-custom-drop">
+                <Link to="/" className="link-logo">
+                    <div className="w-72 m-2">
+                        <img
+                            src="Anime-Almanacs-logo.png"
+                            className="w-full drop-shadow-md"
+                        />
+                    </div>
+                </Link>
+                <NavButtons />
                 <div>
                     <h2>Search</h2>
                 </div>
             </nav>
-            <div>{children}</div>
+
+            <Outlet />
         </>
+    );
+}
+
+function NavButtons() {
+    return (
+        <div className="flex justify-around p-2">
+            <h2 className="text-2xl font-heading text-[#B0B0B0] p-4">Home</h2>
+            <h2 className="text-2xl font-heading text-[#B0B0B0] p-4">
+                TV Shows
+            </h2>
+            <h2 className="text-2xl font-heading text-[#B0B0B0] p-4">Movies</h2>
+            <h2 className="text-2xl font-heading text-[#B0B0B0] p-4">
+                My Favorites
+            </h2>
+        </div>
     );
 }
