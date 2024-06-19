c4599553d5463f5a0e6e412e2d305317172684d9
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-06-19 13:57:43 -0700

    Adjust mobile response footer, header, logo and title card sizes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
         <>
             <nav className="flex fixed top-0 w-full z-50 justify-center lg:justify-between bg-custom-gradient-2 shadow-custom-drop">
                 <Link to="/" className="link-logo">
-                    <div className="w-72 m-2">
+                    <div className="w-64 sm:w-72 m-1 sm:m-2">
                         <img
                             src="Anime-Almanacs-logo.png"
                             className="w-full drop-shadow-md"
@@ -16,7 +16,7 @@ export default function Header() {
                     <DesktopNavigation />
                 </div>
             </nav>
-            <div className="flex lg:hidden justify-around bg-custom-gradient-5 items-center mx-auto w-[90%] max-w-[425px] sm:w-[425px] md:w-[65%] md:max-w-[65%]  mt-[6.5rem] rounded-lg *:text-[#B0B0B0] *:text-2xl *:font-heading *:p-3">
+            <div className="flex lg:hidden justify-around bg-custom-gradient-5 items-center mx-auto w-[80%] max-w-[350px] sm:max-w-[425px] md:w-[65%] md:max-w-[65%] mt-[5.5rem] sm:mt-[6.5rem] rounded-lg *:text-[#B0B0B0] *:text-xl sm:*:text-2xl *:font-heading *:p-3">
                 <FilterButtons />
             </div>
 
--- src/components/TitleCard.tsx
+++ src/components/TitleCard.tsx
@@ -6,30 +6,22 @@ type TitleCardProps = {
 };
 
 export default function TitleCard({ title }: TitleCardProps) {
-  // function shortenTitle(title: string) {
-  //   const maxLength = 28;
-  //   if (title.length <= maxLength) {
-  //     return title;
-  //   } else {
-  //     return title.substring(0, maxLength) + '...';
-  //   }
-  // }
 
   return (
-    <div>
-      <div className="w-44 mx-2">
+    <>
+      <div className="w-36 sm:w-44 mx-2">
         <img
           src={title.images.jpg.image_url}
           alt={`${title.title_english} image`}
-          className="w-full h-56 rounded-t-md"
+          className="w-full h-48 sm:h-56 rounded-t-md"
         />
       </div>
-      <div className="bg-[rgba(78,54,54,0.85)] mx-2 h-24 shadow-custom-inset rounded-b-md ">
-        <p className="p-2 mx-auto w-40 cursor-pointer text-[rgb(176,176,176)] tracking-wider multiline-truncate h-full">
+      <div className="bg-[rgba(78,54,54,0.85)] mx-2 h-20 sm:h-24 shadow-custom-inset rounded-b-md ">
+        <p className="px-2.5 py-1 sm:p-2 mx-auto w-36 sm:w-40 cursor-pointer text-sm sm:text-base text-[rgb(176,176,176)] tracking-wider multiline-truncate h-full">
           {title.title_english}
         </p>
       </div>
-    </div>
+    </>
   );
 }
 
