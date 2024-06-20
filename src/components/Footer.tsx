65f35ef8baeb030d2075392eca7eb928ca8620cb
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-06-20 10:42:30 -0700

    Adjust so that mobile view starts at small screen breakpoint instead of medium

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ import { IoSearch } from "react-icons/io5";
 export default function Footer() {
     return (
         <>
-            <nav className="lg:hidden flex fixed bottom-0 w-full z-50 justify-center bg-custom-gradient-1 shadow-custom-drop">
-                <div className="p-1.5 md:p-2 w-[80%] max-w-[400px] sm:max-w-[425px] md:w-[65%] md:max-w-[65%]">
-                    <div className="flex justify-center bg-custom-gradient-4 rounded-lg *:text-[#B0B0B0] *:size-11 md:*:size-[3.2rem] *:px-1.5 md:*:px-2 *:mx-4 sm:*:mx-6 md:*:mx-10 *:drop-shadow-xl">
+            <nav className="md:hidden flex fixed bottom-0 w-full z-50 justify-center bg-custom-gradient-1 shadow-custom-drop">
+                <div className="p-1.5 w-[80%] max-w-[400px] sm:max-w-[425px] md:w-[65%] md:max-w-[65%]">
+                    <div className="flex justify-center bg-custom-gradient-4 rounded-lg *:text-[#B0B0B0] *:size-11 *:px-1.5 *:mx-4 sm:*:mx-6 *:drop-shadow-xl">
                         <MobileNavigation />
                     </div>
                 </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from 'react-router-dom';
 export default function Header() {
     return (
         <>
-            <nav className="flex lg:fixed top-0 w-full z-50 lg:justify-between bg-custom-gradient-2 shadow-custom-drop">
+            <nav className="flex lg:fixed top-0 w-full z-50 md:justify-between bg-custom-gradient-2 shadow-custom-drop">
                 <Link to="/" className="link-logo">
                     <div className="w-52 md:w-60 lg:w-72 m-1 lg:m-2">
                         <img
@@ -12,11 +12,11 @@ export default function Header() {
                         />
                     </div>
                 </Link>
-                <div className="lg:flex hidden justify-around items-center p-2 *:text-[#B0B0B0] *:text-2xl *:font-heading *:p-4">
+                <div className="md:flex hidden justify-around items-center p-2 *:text-[#B0B0B0] md:*:text-xl lg:*:text-2xl *:font-heading *:p-4">
                     <DesktopNavigation />
                 </div>
             </nav>
-            <div className="flex lg:hidden justify-around bg-custom-gradient-5 items-center mx-auto w-[60%] max-w-[325px] md:w-[65%] md:max-w-[375px] mt-[1rem] md:mt-[1.5rem] rounded-lg *:text-[#B0B0B0] *:text-lg md:*:text-xl *:font-heading *:p-1.5">
+            <div className="flex md:hidden justify-around bg-custom-gradient-5 items-center mx-auto w-[60%] max-w-[325px] mt-[1rem] rounded-lg *:text-[#B0B0B0] *:text-lg *:font-heading *:p-1.5">
                 <FilterButtons />
             </div>
 
