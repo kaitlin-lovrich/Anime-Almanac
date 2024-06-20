4eb17d7c973951a0f580d88359cb887f14cf054e
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-06-20 10:02:18 -0700

    Adjust footer sizing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,8 @@ export default function Footer() {
     return (
         <>
             <nav className="lg:hidden flex fixed bottom-0 w-full z-50 justify-center bg-custom-gradient-1 shadow-custom-drop">
-                <div className="p-3 w-[85%] max-w-[400px] sm:max-w-[425px] md:w-[65%] md:max-w-[65%]">
-                    <div className="flex justify-center bg-custom-gradient-4 rounded-lg *:text-[#B0B0B0] *:size-14 sm*:size-16 *:px-3 *:mx-4 sm:*:mx-6 md:*:mx-10 *:drop-shadow-xl">
+                <div className="p-1.5 md:p-2 w-[80%] max-w-[400px] sm:max-w-[425px] md:w-[65%] md:max-w-[65%]">
+                    <div className="flex justify-center bg-custom-gradient-4 rounded-lg *:text-[#B0B0B0] *:size-11 md:*:size-[3.2rem] *:px-1.5 md:*:px-2 *:mx-4 sm:*:mx-6 md:*:mx-10 *:drop-shadow-xl">
                         <MobileNavigation />
                     </div>
                 </div>
