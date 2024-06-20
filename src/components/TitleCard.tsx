07622e493647f6602b0a80cf23fa876050b73142
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-06-19 18:53:08 -0700

    Adjusted tile and loading cards to shrink for smaller screens

--- src/components/TitleCard.tsx
+++ src/components/TitleCard.tsx
@@ -9,15 +9,15 @@ export default function TitleCard({ title }: TitleCardProps) {
 
   return (
     <>
-      <div className="w-36 sm:w-44 mx-2">
+      <div className="w-36 md:w-40 lg:w-44 mx-2">
         <img
           src={title.images.jpg.image_url}
           alt={`${title.title_english} image`}
-          className="w-full h-48 sm:h-56 rounded-t-md"
+          className="w-full h-48 md:h-52 lg:h-56 rounded-t-md"
         />
       </div>
-      <div className="bg-[rgba(78,54,54,0.85)] mx-2 h-20 sm:h-24 shadow-custom-inset rounded-b-md ">
-        <p className="px-2.5 py-1 sm:p-2 mx-auto w-36 sm:w-40 cursor-pointer text-sm sm:text-base text-[rgb(176,176,176)] tracking-wider multiline-truncate h-full">
+      <div className="bg-[rgba(78,54,54,0.85)] mx-2 w-36 md:w-40 lg:w-44 h-20 md:h-24 shadow-custom-inset rounded-b-md">
+        <p className="px-3 py-1 md:py-2 mx-auto cursor-pointer text-sm md:text-base text-[rgb(176,176,176)] tracking-wider multiline-truncate h-full">
           {title.title_english}
         </p>
       </div>
@@ -25,4 +25,3 @@ export default function TitleCard({ title }: TitleCardProps) {
   );
 }
 
-// Make ttile cards smaller at mobile, and md screen sizes
