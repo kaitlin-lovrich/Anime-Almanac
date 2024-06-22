16ef51d621a6f8fc3ab85e511c3f62a9a1aaa2a8
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-06-22 16:52:35 -0700

    Fix image size on TitlePage

--- src/pages/TitlePage.tsx
+++ src/pages/TitlePage.tsx
@@ -17,14 +17,14 @@ export default function TitlePage() {
         <>
             <div className="flex justify-center flex-col mx-auto mt-8 lg:mt-28 mb-24 w-[90%] lg:w-[85%] lg:max-w-[1380px] text-custom-white gap-4 md:gap-8">
                 <div className="flex flex-col md:flex-row gap-4 md:gap-8 text-lg ">
-                    <section className="w-full max-w-[350px] md:w-80 self-center md:self-start">
+                    <section className="w-full max-w-[350px] md:w-1/2 md:max-w-[380px] lg:w-2/5 lg:max-w-[400px] xl:max-w-[430px] self-center md:self-start">
                         <img
                             src={title.images.jpg.image_url}
                             alt={`${title.title_english} image`}
                             className="w-full rounded"
                         />
                     </section>
-                    <div className="flex flex-col justify-between gap-4">
+                    <div className="flex flex-col justify-between md:w-1/2 gap-4">
                         <aside className="flex flex-col *:py-1.5 md:pt-24">
                             <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">
                                 {title.title_english}
