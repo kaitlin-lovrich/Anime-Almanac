e9568b517fe3e4535fdc4b9997e6d8c26e48d0f3
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-06-21 21:51:15 -0700

    WIP TitlePage CSS

--- src/components/TitleCard.tsx
+++ src/components/TitleCard.tsx
@@ -1,30 +1,28 @@
-import { TitleData } from '../lib/dataTypes';
-import '../index.css';
-import { Link } from 'react-router-dom';
+import { TitleData } from "../lib/dataTypes";
+import "../index.css";
+import { Link } from "react-router-dom";
 
 type TitleCardProps = {
-  title: TitleData;
+    title: TitleData;
 };
 
 export default function TitleCard({ title }: TitleCardProps) {
-
-  return (
-    <>
-      <div className="w-36 md:w-40 lg:w-44 mx-2">
-        <img
-          src={title.images.jpg.image_url}
-          alt={`${title.title_english} image`}
-          className="w-full h-48 md:h-52 lg:h-56 rounded-t-md"
-        />
-      </div>
-      <div className="bg-[rgba(78,54,54,0.85)] mx-2 w-36 md:w-40 lg:w-44 h-20 md:h-24 shadow-custom-inset rounded-b-md">
-          <Link to={`title-page/${title.mal_id}`} state={{ title }}>
-              <p className="px-3 py-1 md:py-2 mx-auto cursor-pointer text-sm md:text-base text-[rgb(176,176,176)] tracking-wider multiline-truncate h-full">
-                {title.title_english}
-              </p>
-          </Link>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="w-36 md:w-40 lg:w-44 mx-2">
+                <img
+                    src={title.images.jpg.image_url}
+                    alt={`${title.title_english} image`}
+                    className="w-full h-48 md:h-52 lg:h-56 rounded-t-md"
+                />
+            </div>
+            <div className="bg-[rgba(78,54,54,0.85)] mx-2 w-36 md:w-40 lg:w-44 h-20 md:h-24 shadow-custom-inset rounded-b-md">
+                <Link to={`title-page/${title.mal_id}`} state={{ title }}>
+                    <p className="px-3 py-1 md:py-2 mx-auto cursor-pointer text-sm md:text-base text-[rgb(176,176,176)] tracking-wider multiline-truncate-3 h-full">
+                        {title.title_english}
+                    </p>
+                </Link>
+            </div>
+        </>
+    );
 }
-
--- src/pages/TitlePage.tsx
+++ src/pages/TitlePage.tsx
@@ -3,10 +3,6 @@ import { TitleData } from "../lib/dataTypes";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 
-// type TitlePageProps = {
-//   title: TitleData
-// }
-
 export default function TitlePage() {
     const [isSaved, setisSaved] = useState(false);
     const location = useLocation();
@@ -18,32 +14,50 @@ export default function TitlePage() {
 
     return (
         // Change width
-        <div className="flex justify-center flex-col w-3/4">
-            <section className="w-80">
-                <img
-                    src={title.images.jpg.image_url}
-                    alt={`${title.title_english} image`}
-                    className="w-full rounded"
-                />
-            </section>
-            <aside>
-                <h1>{title.title_english}</h1>
-                <p>{title.genres.map((genre) => genre).join(" ")}</p>
-                <p>
-                    {title.year} {title.rating} {title.type}
-                </p>
-                <p>
-                    {title.episodes} {title.duration}
-                </p>
-            </aside>
-
-            {isSaved ? (
-                <FaRegHeart onClick={() => setisSaved(!isSaved)} />
-            ) : (
-                <FaHeart onClick={() => setisSaved(!isSaved)} />
-            )}
-
-            <p>{title.synopsis}</p>
+        <div className="flex justify-center flex-col mx-auto md:mt-8 lg:mt-28 w-[90%] lg:w-[85%] lg:max-w-[1380px] text-custom-white gap-8">
+            <div className="flex gap-8 text-lg">
+                <section className="w-80">
+                    <img
+                        src={title.images.jpg.image_url}
+                        alt={`${title.title_english} image`}
+                        className="w-full rounded"
+                    />
+                </section>
+                <div className="flex flex-col justify-between">
+                    <aside className="flex flex-col *:py-1.5 pt-24">
+                        <h1 className="text-5xl font-bold">
+                            {title.title_english} Attack on Titan
+                        </h1>
+                        <p>
+                            {title.genres
+                                .map((genre, index) => genre[index])
+                                .join(" ")}
+                        </p>
+                        <p>
+                            {title.year}{" "}
+                            <span className="font-light px-2 font-base">
+                                {title.rating}
+                            </span>{" "}
+                            {title.type === "TV"
+                                ? `${title.type} Series`
+                                : title.type}
+                        </p>
+                        <p>
+                            {title.episodes} episodes {title.duration}isode
+                        </p>
+                    </aside>
+                    <span className="*:size-10 *:cursor-pointer hover:*:scale-110 active:*:scale-110 *:duration-300">
+                        {isSaved ? (
+                            <FaHeart onClick={() => setisSaved(!isSaved)} />
+                        ) : (
+                            <FaRegHeart onClick={() => setisSaved(!isSaved)} />
+                        )}
+                    </span>
+                </div>
+            </div>
+            <p className="text-2xl text-justify multiline-truncate-6 sm:text-red-200 md:text-green-200 lg:text-red-800 xl:text-yellow-600">
+                {title.synopsis}
+            </p>
         </div>
     );
 }
