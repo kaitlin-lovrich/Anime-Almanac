1cacccf8537f554b7f5b6b3bd54d9231693bd0be
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-06-22 17:30:17 -0700

    Adjust mobile CSS TP

--- src/pages/TitlePage.tsx
+++ src/pages/TitlePage.tsx
@@ -15,16 +15,16 @@ export default function TitlePage() {
 
     return (
         <>
-            <div className="flex justify-center flex-col mx-auto mt-8 lg:mt-28 mb-24 w-[90%] lg:w-[85%] lg:max-w-[1380px] text-custom-white gap-4 md:gap-8">
+            <div className="flex justify-center flex-col mx-auto mt-8 lg:mt-28 mb-24 w-[90%] lg:w-[85%] lg:max-w-[1380px] text-custom-white gap-5 md:gap-8">
                 <div className="flex flex-col md:flex-row gap-4 md:gap-8 text-lg ">
-                    <section className="w-[80%] max-w-[350px] md:w-1/2 md:max-w-[380px] lg:w-2/5 lg:max-w-[400px] xl:max-w-[430px] self-center md:self-start">
+                    <section className="w-[75%] max-w-[350px] md:w-1/2 md:max-w-[380px] lg:w-2/5 lg:max-w-[400px] xl:max-w-[430px] self-center md:self-start">
                         <img
                             src={title.images.jpg.image_url}
                             alt={`${title.title_english} image`}
                             className="w-full rounded"
                         />
                     </section>
-                    <div className="flex flex-col justify-between md:w-1/2 gap-4">
+                    <div className="flex flex-col justify-between md:w-1/2 gap-5">
                         <aside className="flex flex-col *:py-1.5 md:pt-24">
                             <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">
                                 {title.title_english}
@@ -48,7 +48,7 @@ export default function TitlePage() {
                                 {title.duration}isode
                             </p>
                         </aside>
-                        <span className="*:size-9 xl:*:size-10 *:cursor-pointer hover:*:scale-110 active:*:scale-110 *:duration-300">
+                        <span className="*:size-10 *:cursor-pointer hover:*:scale-110 active:*:scale-110 *:duration-300">
                             {isSaved ? (
                                 <FaHeart onClick={() => setisSaved(!isSaved)} />
                             ) : (
@@ -59,7 +59,7 @@ export default function TitlePage() {
                         </span>
                     </div>
                 </div>
-                <p className="text-xl lg:text-2xl text-justify lg:leading-[2.8rem] multiline-truncate-2">
+                <p className="text-xl lg:text-2xl sm:text-justify lg:leading-[2.8rem] multiline-truncate-2">
                     {title.synopsis}
                 </p>
             </div>
