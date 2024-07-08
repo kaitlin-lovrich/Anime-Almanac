5e562cc573955c6c67dfbb837d1741aec891c2d2
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-07-08 16:08:24 -0700

    #6 Need to test on deployed site if footer keyboard bug gets fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,19 +27,6 @@ export default function Footer() {
         }
     }, [isInputFocussed, isSearchBarIconClicked]);
 
-    return (
-        <div ref={footerRef}>
-            <MobileNavigation />
-        </div>
-    );
-}
-
-function MobileNavigation() {
-    const { filter, setFilter } = useContext(AppContext);
-    const [isSearchBarOpen, setIsSearchBarOpen] = useState(false);
-
-    const footerRef = useRef<HTMLDivElement | null>(null);
-
     useEffect(() => {
         let focusTimeout: NodeJS.Timeout;
 
@@ -84,69 +71,74 @@ function MobileNavigation() {
     }, []);
 
     return (
-        <>
-            <nav
-                ref={footerRef}
-                className="md:hidden flex items-center fixed bottom-0 w-full z-50 justify-center bg-custom-gradient-1 shadow-custom-drop"
-            >
-                {isSearchBarOpen && (
-                    <span>
-                        <FaChevronLeft
-                            onClick={() => setIsSearchBarOpen(!isSearchBarOpen)}
-                            className="pr-3 size-8 text-custom-gray hover:cursor-pointer hover:scale-110 hover:text-custom-white duration-300"
-                        />
-                    </span>
-                )}
-                <div className="p-1.5 w-[80%] max-w-[400px] sm:max-w-[425px] md:w-[65%] md:max-w-[65%]">
-                    <div
-                        className={`${
-                            isSearchBarOpen
-                                ? `focus-within:ring-2 focus-within:ring-custom-gray`
-                                : `justify-center *:*:mx-4`
-                        } flex bg-custom-gradient-4 rounded-lg text-custom-gray *:*:size-14 *:*:px-1.5  sm:*:*:mx-6 *:drop-shadow-xl *:cursor-pointer hover:*:text-custom-white hover:*:scale-110 active:*:scale-110 *:duration-300`}
-                    >
-                        {!isSearchBarOpen && (
-                            <Link to="/">
-                                <HiMiniHome
-                                    onClick={() => setFilter("Home")}
-                                    className={
-                                        filter === "Home"
-                                            ? "text-custom-white"
-                                            : ""
-                                    }
-                                />
-                            </Link>
-                        )}
+        <nav
+            ref={footerRef}
+            className="md:hidden flex items-center fixed bottom-0 w-full z-50 justify-center bg-custom-gradient-1 shadow-custom-drop"
+        >
+            <MobileNavigation />
+        </nav>
+    );
+}
 
-                        {!isSearchBarOpen && (
-                            <span
-                                onClick={() =>
-                                    setIsSearchBarOpen(!isSearchBarOpen)
+function MobileNavigation() {
+    const { filter, setFilter } = useContext(AppContext);
+    const [isSearchBarOpen, setIsSearchBarOpen] = useState(false);
+
+    return (
+        <>
+            {isSearchBarOpen && (
+                <span>
+                    <FaChevronLeft
+                        onClick={() => setIsSearchBarOpen(!isSearchBarOpen)}
+                        className="pr-3 size-8 text-custom-gray hover:cursor-pointer hover:scale-110 hover:text-custom-white duration-300"
+                    />
+                </span>
+            )}
+            <div className="p-1.5 w-[80%] max-w-[400px] sm:max-w-[425px] md:w-[65%] md:max-w-[65%]">
+                <div
+                    className={`${
+                        isSearchBarOpen
+                            ? `focus-within:ring-2 focus-within:ring-custom-gray`
+                            : `justify-center *:*:mx-4`
+                    } flex bg-custom-gradient-4 rounded-lg text-custom-gray *:*:size-14 *:*:px-1.5  sm:*:*:mx-6 *:drop-shadow-xl *:cursor-pointer hover:*:text-custom-white hover:*:scale-110 active:*:scale-110 *:duration-300`}
+                >
+                    {!isSearchBarOpen && (
+                        <Link to="/">
+                            <HiMiniHome
+                                onClick={() => setFilter("Home")}
+                                className={
+                                    filter === "Home" ? "text-custom-white" : ""
                                 }
-                            >
-                                <IoSearch />
-                            </span>
-                        )}
-                        {isSearchBarOpen && (
-                            <div className="flex no-hover-effect">
-                                <SearchTitles mobile={true} />
-                            </div>
-                        )}
-                        {!isSearchBarOpen && (
-                            <Link to="/my-favorites">
-                                <FaRegHeart
-                                    onClick={() => setFilter("My Favorites")}
-                                    className={
-                                        filter === "My Favorites"
-                                            ? "text-custom-white"
-                                            : ""
-                                    }
-                                />
-                            </Link>
-                        )}
-                    </div>
+                            />
+                        </Link>
+                    )}
+
+                    {!isSearchBarOpen && (
+                        <span
+                            onClick={() => setIsSearchBarOpen(!isSearchBarOpen)}
+                        >
+                            <IoSearch />
+                        </span>
+                    )}
+                    {isSearchBarOpen && (
+                        <div className="flex no-hover-effect">
+                            <SearchTitles mobile={true} />
+                        </div>
+                    )}
+                    {!isSearchBarOpen && (
+                        <Link to="/my-favorites">
+                            <FaRegHeart
+                                onClick={() => setFilter("My Favorites")}
+                                className={
+                                    filter === "My Favorites"
+                                        ? "text-custom-white"
+                                        : ""
+                                }
+                            />
+                        </Link>
+                    )}
                 </div>
-            </nav>
+            </div>
         </>
     );
 }
