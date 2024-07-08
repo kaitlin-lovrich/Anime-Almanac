a4795272c2a0be7f145765749d72f50cad6c884e
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-07-08 16:15:25 -0700

    WIP Footer keyboard broke when first useEffect adjusted from 3000 to 300, trying 600 now

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,7 +23,7 @@ export default function Footer() {
                         window.innerHeight - height
                     }px`;
                 }
-            }, 300);
+            }, 600);
         }
     }, [isInputFocussed, isSearchBarIconClicked]);
 
